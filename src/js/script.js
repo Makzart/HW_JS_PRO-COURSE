3d3ae794603013150208dbebdd2adc4dc70b0997
Makzart <max.artyukhov.14@gmail.com>
2023-12-16 22:13:57 +0200

    HW 3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const a = prompt('Enter the first number');
+const b = prompt('Enter the second number');
+
+const numA = Number(a);
+const numB = Number(b);
+
+if (!isNaN(numA) && !isNaN(numB)) {
+    console.log('Result: '+ (numA + numB));
+    console.log('Result: '+ (numA - numB));
+    console.log('Result: '+ (numA * numB));
+    console.log('Result: '+ (numA / numB));
+} else {
+    alert('Invalid input. Please enter valid numbers.');
+}
+
