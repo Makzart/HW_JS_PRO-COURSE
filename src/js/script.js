c964f8c1be3b28c189003fc344905e905910b05c
Makzart <max.artyukhov.14@gmail.com>
2023-12-16 21:09:10 +0200

    HW 2 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+//Task 1
+
+let name;
+let admin;
+name = 'John';
+admin = name;
+alert(admin);
+
+//Task 2
+
+const planetEarth = 'Earth';
+const userName = 'User Name';
+
+//Task 3
+
+const example = '10' + 10;
+console.log(example);
+console.log(typeof example);
