d485cc23c06bcbfaec93e1d3c78bf82ee2e00497
Makzart <max.artyukhov.14@gmail.com>
2023-12-19 20:32:24 +0200

    HW 4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+/*Task #1*/
+
+const firstString = prompt('Enter first string');
+const secondString = prompt('Enter second string');
+const thirdString = prompt('Enter third string');
+
+const result = secondString + " " + thirdString + " " + firstString;
+
+console.log(result);
+
+
+/*Task #2*/
+const numberExample = 23462;
+const numberString = String(numberExample);
+
+const resultTask2 = `Number: ${numberString[0] + " " + numberString[1] + " " + numberString[2] + " " + numberString[3] + " " + numberString[4]}`;
+
+console.log(resultTask2);
