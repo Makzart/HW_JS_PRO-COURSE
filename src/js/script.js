971882c40a6a5595eadd5e881b9df88b38cd0866
Makzart <max.artyukhov.14@gmail.com>
2024-01-03 18:23:37 +0200

    HW 14 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const hours = Number(prompt("Введіть кількість годин:"));
+
+if (isNaN(hours) || hours < 0) {
+    alert("Будь ласка, введіть коректну кількість годин.");
+} else {
+    const seconds = hours * 3600;
+
+    alert("Кількість секунд у " + hours + " годинах: " + seconds + " секунд.");
+}
