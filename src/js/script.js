a59d2b765eb06004ff5b721bd98e2dc0897fa02b
Makzart <max.artyukhov.14@gmail.com>
2024-01-03 18:32:19 +0200

    HW 15 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const firstNumber = Number(prompt("Введіть перше число"));
+const secondNumber = Number(prompt("Введіть друге число"));
+const thirdNumber = Number(prompt("Введіть третє число"));
+
+if (isNaN(firstNumber) || isNaN(secondNumber) || isNaN(thirdNumber)) {
+    alert("Будь ласка, введіть коректні числа.");
+} else {
+    const average = (firstNumber + secondNumber + thirdNumber) / 3;
+
+    alert('Середнє арифметичне цих чисел: ' + average);
+}
