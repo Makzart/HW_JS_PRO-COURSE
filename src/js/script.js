c16b2c46deaf8428aed7644f52c9f6d2ca602a62
Makzart <max.artyukhov.14@gmail.com>
2024-01-03 17:43:53 +0200

    HW 12 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const firstNumber = Number(prompt('Введіть перше число:'));
+const secondNumber = Number(prompt('Введіть друге число:'));
+
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert("Будь ласка, введіть коректні числа.");
+} else {
+    const addition = firstNumber + secondNumber;
+    const subtraction = firstNumber - secondNumber;
+    const multiplication = firstNumber * secondNumber;
+    const division = firstNumber / secondNumber;
+
+    const resultMessage = `Користувач ввів ${firstNumber} і ${secondNumber}:\n` +
+        firstNumber + " + " + secondNumber + " = " + addition + "\n" +
+        firstNumber + " - " + secondNumber + " = " + subtraction + "\n" +
+        firstNumber + " * " + secondNumber + " = " + multiplication + "\n" +
+        firstNumber + " / " + secondNumber + " = " + division;
+
+    alert(resultMessage);
+}
+
