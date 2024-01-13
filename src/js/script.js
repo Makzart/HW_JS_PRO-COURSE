cc21a91cff762397b46aac0ca6002ea35e95b381
Makzart <max.artyukhov.14@gmail.com>
2024-01-14 01:56:44 +0200

    HW 27 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const userNumber = Number(prompt('Введіть число: '));
+const userDegree = Number(prompt('Введіть ступінь числа: '));
+
+function calculation(number, degree = 1) {
+    if (Number.isNaN(number) || Number.isNaN(degree)) {
+        return alert("Помилка");
+    }
+        else {
+            return Math.pow(number, degree);
+    }
+}
+
+const result = calculation (userNumber, userDegree);
+alert("Ось ваше число: " + result);
+
+
+
