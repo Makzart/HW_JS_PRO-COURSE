211eb5b3492a90020c69de25d27db4fcf3092817
Makzart <max.artyukhov.14@gmail.com>
2024-01-17 01:01:38 +0200

    HW 17 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+const numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch (true) {
+    case (numOrStr === null):
+        console.log('ви скасували');
+        break;
+    case (numOrStr.trim() === ''):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+}
