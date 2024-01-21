2aeb58b2163ae03fdcb3748b423c470dd9c9bbe5
Makzart <max.artyukhov.14@gmail.com>
2024-01-22 00:36:51 +0200

    removed else

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,8 @@ const userNumber = Number(prompt('Введіть число: '));
 const userDegree = Number(prompt('Введіть ступінь числа: '));
 
 function calculation(number, degree = 1) {
-    if (Number.isNaN(number) || Number.isNaN(degree)) {
-        return alert("Помилка");
-    }
-        else {
-            return Math.pow(number, degree);
-    }
+    if (Number.isNaN(number) || Number.isNaN(degree)) return alert("Помилка");
+    return Math.pow(number, degree);
 }
 
 const result = calculation (userNumber, userDegree);
