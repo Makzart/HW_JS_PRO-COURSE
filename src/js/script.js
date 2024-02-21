90ade4763d1d28485aee2c4d706111937ec7ab5d
Makzart <max.artyukhov.14@gmail.com>
2024-02-21 18:39:38 +0200

    hw 48 completed

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+/* Example #1 */
+const sum = (a, b) => a + b;
+
+console.log(sum(4, 5));
+/* Example #2 */
+const strLength = (string) => string.length;
+
+console.log(strLength('Hillel'));
+/* Example #3 */
+const incrementArray = (arr) => arr.map((num) => num + 1);
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(incrementArray(numbers));
